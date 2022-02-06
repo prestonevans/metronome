ad333d082718d7ee33595dba38c3232b25b415d9
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-06 12:45:26 -0700

    basic metronome needs tons of work

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
 const onOff = document.querySelector('input[value="On/Off"')
 const bpm = document.querySelector('select')
 const metronome = document.querySelector('.click')
+let timer;
+
+async function playClick(bpm=1000) {
+    const clickAudio = await new Audio('/audio/PUCK1.wav') 
+    clickAudio.play()
+    timer = setInterval(() => {
+        clickAudio.play()
+    },bpm)
+}
+
+playClick()
 
 onOff.addEventListener('click', () => {
+    const convertToMS = +(60000/bpm.value).toFixed(2)
     metronome.classList.toggle('animation')
+    metronome.classList.contains('animation') ?
+        playClick(convertToMS) : clearInterval(timer)
 } )
 
 bpm.addEventListener('change', () => {
-    const convertToMS = (60000/bpm.value).toFixed(2)
+    let status = metronome.classList.contains('animation');
+    const convertToMS = +(60000/bpm.value).toFixed(2)
     metronome.style.animationDuration = `${convertToMS}ms`
+    if (!status)  return
+    clearInterval(timer)
+    playClick(convertToMS)
 })
