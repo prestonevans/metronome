eee1cc61da29d8be2e51bdff40d639ef1995059a
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-06 11:07:22 -0700

    basic metronome visuals still need to add sound

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const onOff = document.querySelector('input[value="On/Off"')
+const bpm = document.querySelector('select')
+const metronome = document.querySelector('.click')
+
+onOff.addEventListener('click', () => {
+    metronome.classList.toggle('animation')
+} )
+
+bpm.addEventListener('change', () => {
+    const convertToMS = (60000/bpm.value).toFixed(2)
+    metronome.style.animationDuration = `${convertToMS}ms`
+})
